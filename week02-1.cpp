ff3b8ddd0cb0a4b83d46c6ff1d748df9f8b24299
11160356 <rock963700@gmail.com>
2022-11-11 12:12:13 +0800

    week08

--- week02-1.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("Hello World\n");
-    print("Hello%dWorld\n",3);
-}
---
+++ week02/week02-1.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    print("Hello%dWorld\n",3);
+}
